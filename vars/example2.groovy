da104e63aafcc37fe0f4c9056509d763e0578ebb
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-05-31 18:12:53 +0530

    Create example2.groovy

---
+++ vars/example2.groovy
@@ -0,0 +1,12 @@
+def call(Closure postHelloScript) {
+    node {
+        stage("Hello World") {
+            script {
+                sh """
+                    echo Hello
+                """
+            }
+        }
+        postHelloScript()
+    }
+}
